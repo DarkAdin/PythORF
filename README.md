87e512db44d9619925c30f7ff1802181f5773f9f
DarkAdin <adansomoza@outlook.com>
2020-04-07 00:08:06 +0200

    More interactive algorithm

--- README.md
+++ README.md
@@ -1,32 +1,32 @@
 # Sequencetool
-A little, simple, humble Python algorithm that finds all possible Open Reading Frames within a given DNA sequence, and finds some arbitrary targets.
+A little Python algorithm that finds all possible Open Reading Frames within a given DNA sequence, and finds some arbitrary targets.
 
-This algorithm tries to emulate the results and reliability of the ORF Finder from NCBI, while avoiding the use of any Python module to keep it clean and self-reliant. It should be noticeably fast on any regular sequence and the CPU shouldn't even sweat, although it'd be advisable to avoid very large sequences (more than 100 kb).
+This algorithm tries to emulate the results and reliability of the ORF Finder from NCBI, while avoiding the use of any additional Python module to keep it clean and self-reliant. It should be noticeably fast on any regular coding sequence and the CPU shouldn't even sweat, although it'd be advisable to avoid very large sequences (more than 100 Kb).
 
 ## Functionalities
 
 ### Transcription and translation
-Using Python's built-in method of creating dictionaries, the algorithm relies on two of them to fully transcribe and translate the sequence: one for transcription, and one for translation (which includes the full 'universal' genetic code).
+Using Python's built-in method of creating dictionaries, the algorithm relies on three of them to fully transcribe and translate the sequence: one for transcription, one for translation (which includes the full 'universal' genetic code). The third one serves the only purpose of generating a complementary DNA for the *plus* frames.
 
-Using these two dictionaries and a very compacted nested 'for' loop, two mRNAs are generated on the fly, one for 'plus', one for 'minus' frames, and translated into as many peptides as the current frame contains. This tries to avoid explicit list declaration, using as little memory as possible. In Python, this should go always first.
+Using these three dictionaries and two very compacted nested 'for' loops, 6 mRNAs are generated on the fly, one for *plus*, one for *minus* frames, and translated into as many peptides as the current frame contains. This tries to avoid explicit list declaration, using as little memory as possible.
 
 After finding a peptide that's long enough for the open reading frame's length (which can be set by the user), it is printed on the screen, and the loop jumps to the next.
 
 The loop goes on until all 6 frames are finished. Empty frames are also printed on screen.
 
 ### Genomic targets
-Since it's a common tool in genomics, there's also an option to find every possible genomic target on the DNA sequence. The entire DNA sequence is scanned and every target printed on screen in another 'for' loop along with its position.
+Since it's a common tool in genomics, there's also an option to find every possible genomic target on the DNA sequence. The entire DNA sequence is scanned and every target printed on screen in another 'for' loop along with its exact position.
 
 ## Purpose
-The main purpose of this algorithm is to bring the most common tools both to unexperienced and experienced students and academics in the field of Molecular Biology and Genomics, without having to depend on some website, third party organization, or licensed application.
+The main purpose of this algorithm is to bring the most common tools both to unexperienced and experienced students and academics in the field of Molecular Biology and Genomics, without having to depend on some website, third party organization, or licensed application. The massive use of licensed applications plagues the entire academic field and personally is something I'd have loved to avoid.
 Also, Python is a great language.
 
 ## To do
 This little tool is far from finished, far from polished. Things to be added:
 * Make the user decide as many parameters as possible, for example whether to just find open reading frames or just find genomic targets, without having to do one, then the other.
-* Allow starting from RNA.
-* Include a method of reverse transcripting said RNA, if needed.
-* Include a system to rate every possible peptide and present the most probable one, given the context of translation start and ending sites at the mRNA.
+* Allow starting from RNA. Also, there are plenty of cases of ncRNAs that may contain little ORFs, some actually functional.
+* Include a method of reverse transcripting said RNA.
+* Include a system to rate every possible peptide and present the most probable one, given the context of translation start and ending sites at the mRNA. Perhaps include Shine-Dalgarno sites, and options for alternative poli-adenilation and termination.
 * Include a tool to design primers for the amplification of the sequence if possible. Again, the user here should have the last word on deciding the primers best suited for their needs.
 * Make cleaner, even more direct code. Focusing on memory usage, the algorithm should be as efficient as possible, without a single variable, a single list, declared in vain. When writing in Python, this always has to be one of the main concerns.
 * And more...
--- Sequencetool.py
+++ Sequencetool.py
@@ -1,23 +1,45 @@
-DNA=list(open('IFNG.fas','r').read())
-for a in [' ', '\t', '\n']:
+try:
+    DNA=list(open(input('Insert exact filename of DNA sequence to be read: '),'r').read()) #DNA is read from a specific file. User is prompted to type the exact filename of the sequence. FASTA format is not supported yet, since there needs to be a way of dealing with the first line that contains the metadata of the sequence. It will be implemented in time.
+except: #If for some reason the file is invalid, doesn't exist or the path to it is not correct, display an error and exit.
+    print('Invalid file/path. Exiting.')
+    exit()
+for a in [' ', '\t', '\n', '>']: #List of unwanted characters
     while a in DNA:
         DNA.remove(a) #Cleaning up the sequence
-print('75 pb means peptides at least 25 aminoacids long')
+print('The DNA sequence is', len(DNA), 'bp long.')
+Startpos=input('Start position (or leave blank): ') #The user is prompted to decide, if they want to, the start and end positions within the DNA sequence. The default option, of course, is scanning the entire DNA sequence.
+Endpos=input('End position (or leave blank): ')
+try:
+    int(Startpos)
+except:
+    Startpos=0
+try:
+    int(Endpos)
+except:
+    Endpos=len(DNA)-1
+if int(Startpos) not in range(len(DNA)): #If the user leaves them blank or inputs an invalid value outside the range of the DNA itself, they are defaulted to the start and end of the molecule, respectively.
+    Startpos=0
+if Endpos < Startpos or Endpos not in range(len(DNA)-1):
+    Endpos=len(DNA)-1
+DNA=DNA[int(Startpos):int(Endpos)] #Whether the user inputs correct values or not, the sequence is set to be read in the decided range.
+print('Selected DNA between positions:', Startpos,'-', Endpos, '. Selected DNA sequence now is', len(DNA), 'bp long.')
+print('75 pb means peptides at least 25 aminoacids long (default: 75 bp).')
 ORFlength=input('Minimal length of ORF? (pb) ')
-ORFlength=75
+if ORFlength is not int: #If the user inserts anything other than an int, ORF length is defaulted to 75 bp
+        ORFlength=75
 #Dictionaries for: creating a complementary DNA; transcribing the DNA; translating the RNA
 Compl = {
         "A":"T",
         "T":"A",
         "C":"G",
         "G":"C",
-        }
+        } #Dictionary to create a complementary DNA strand
 Transcription = {
         "A":"U",
         "C":"G",
         "G":"C",
         "T":"A",
-        }
+        } #Dictionary to transcribe the DNA
 Translation = {
         "UUU":"F",
         "UUC":"F",
@@ -83,33 +105,36 @@ Translation = {
         "GGC":"G",
         "GGA":"G",
         "GGG":"G",
-        }
+        } #Dictionary that contains the 'universal' genetic code to help translate every RNA into its corresponding protein
+ORFs=0
 for i in range(3):
     print('Frame +',i+1,':')
     Count=0
     Codon=[]
     Protein=[]
-    for ribo in [Transcription[Nuc] for Nuc in [Compl[D] for D in DNA][i:]]:
+    for ribo in [Transcription[Nuc] for Nuc in [Compl[D] for D in DNA][i:]]: #First, the DNA is turned into its complementary version. It is transcribed to RNA right after
         Count+=1
         Codon.append(ribo)
-        if Count==3:
-            Count-=3
-            Protein.append(Translation[''.join(Codon)])
-            del Codon[:]
-    while 'M' in Protein and '*' in Protein:
+        if Count==3: #Ensuring that the codon is exactly 3 ribonucleotids long
+            Count-=3 #Resetting the counter. I feel unconfortable about these counters, but for now I can't seem to find an alternative...
+            Protein.append(Translation[''.join(Codon)]) #Every codon is translated right before appending its corresponding aminoacid to the nascent protein
+            del Codon[:] #Resetting the codon
+    while 'M' in Protein and '*' in Protein: #Making sure the protein contains methionins and stops.
         if Protein.index('M') < Protein.index('*'):
-            if len(Protein[Protein.index('M'):Protein.index('*')])>int(ORFlength)/3:
-                print(3*(len(Protein[Protein.index('M'):Protein.index('*')]))+3, 'nts |', len(Protein[Protein.index('M'):Protein.index('*')]), 'aas:', ''.join(Protein[Protein.index('M'):Protein.index('*')]))
-            Protein.remove('M')
-            Protein.remove('*')
+            if len(Protein[Protein.index('M'):Protein.index('*')])>int(ORFlength)/3: #The chosen minimal ORF Length comes into play. The length of all ORFs must obey its exact value, or higher.
+                print(3*(len(Protein[Protein.index('M'):Protein.index('*')]))+3, 'nts |', len(Protein[Protein.index('M'):Protein.index('*')]), 'aas:', ''.join(Protein[Protein.index('M'):Protein.index('*')])) #The ORF itself! Not more than the two same indexes manipulated in different ways: the index for 'M' and the index for '*', and nothing else.
+                ORFs+=1
+            #Protein.remove('M') #Once the ORF has been shown, remove its beginning and end but not the section in between, which may contain more ORFs ('nested').
+            #Protein.remove('*')
+            del Protein[Protein.index('M'):Protein.index('*')] #This setting deactivates the search for nested ORFs. Comment it and uncomment the two lines above to obtain nested ORFs. The search for nested ORFs is turned off by default in NCBI's ORF Finder as well. Activating it means obtaining a greater number of ORFs.
         else:
-            Protein.remove('*')
+            Protein.remove('*') #Removal of stop codons that might come before any Methionin, interrupting the reading of any ORF.
 for i in range(3):
     print('Frame -',i+1,':')
     Count=0
     Codon=[]
     Protein=[]
-    for ribo in [Transcription[Nuc] for Nuc in DNA[-i-1::-1]]:
+    for ribo in [Transcription[Nuc] for Nuc in DNA[-i-1::-1]]: #DNA is read backwards in all 3 remaining frames, and the rest of the process is exactly the same as above.
         Count+=1
         Codon.append(ribo)
         if Count==3:
@@ -120,30 +145,35 @@ for i in range(3):
         if Protein.index('M') < Protein.index('*'):
             if len(Protein[Protein.index('M'):Protein.index('*')])>int(ORFlength)/3:
                 print(3*(len(Protein[Protein.index('M'):Protein.index('*')]))+3, 'nts |', len(Protein[Protein.index('M'):Protein.index('*')]), 'aas:', ''.join(Protein[Protein.index('M'):Protein.index('*')]))
-            Protein.remove('M')
-            Protein.remove('*')
+                ORFs+=1
+            #Protein.remove('M')
+            #Protein.remove('*')
+            del Protein[Protein.index('M'):Protein.index('*')]
         else:
             Protein.remove('*')
-input('Press any key to find genomic targets...')
+print(ORFs, 'ORFs found.')
+input('Press any key to find genomic targets... ')
 #########
 # TARGETS
 #########
-Contador=0
-Dianas_DNA=['ATG','CGGCGG','GCCGCC']
-for Diana in Dianas_DNA:
-    print('Targets found for ', ''.join(Diana),':')
-    Ind=0
-    Blancos=0
-    for Nucl in DNA:
-        if Nucl == list(Diana)[Contador]:
-            Contador+=1
-            Ind+=1
-        else:
-            Contador=0
-            Ind+=1
-        if Contador == len(list(Diana)):
-            Contador=0
-            Blancos+=1
-            print(Ind-len(list(Diana)), '-', ''.join(Diana), '-', Ind)
-    print(Blancos, 'targets', ''.join(Diana), 'found.')
-    print('--------------')
+def Targets(DNA): #Putting all this in a function to allow the user calling it in future versions, since it is an optional tool and should only prompt when the user chooses.
+    Count=0
+    Targets=['ATG','CGGCGG','GCCGCC']
+    for Target in Targets:
+        print('Targets found for ', ''.join(Target),':')
+        Ind=0
+        Hits=0
+        for Nucl in DNA:
+            if Nucl == list(Target)[Count]:
+                Count+=1
+                Ind+=1
+            else:
+                Count-=Count
+                Ind+=1
+            if Count== len(list(Target)):
+                Count-=Count
+                Hits+=1
+                print(Ind-len(list(Target)), '-', ''.join(Target), '-', Ind)
+        print(Hits,''.join(Target), 'targets found.')
+        print('--------------')
+Targets(DNA)
